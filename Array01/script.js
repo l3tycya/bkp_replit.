064871e5906dfe53831238a3670ca33e36a5cd72
l3tycya <leticordeiro08@gmail.com>
2024-10-17 11:32:32 -0300

    ARRAY01

---
+++ Array01/script.js
@@ -0,0 +1,15 @@
+//VARIAVEIS UNITARIAS
+const nome = 'jonas'
+const nome2 = 'julia'
+const nome3 = 'marcos'
+//...
+
+
+
+//VARIAVEIS DE ARRAY
+const nomes = ['jonas', 'julia', 'marcos']
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = "Dino"
+//CRIANDO DINAMICAMENTE
+console.log(nomes[10])
