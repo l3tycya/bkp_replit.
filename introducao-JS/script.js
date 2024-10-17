1a4a7d0f500a1a48de3aa976bddd11ebff7fe738
l3tycya <leticordeiro08@gmail.com>
2024-10-17 12:16:52 -0300

    INTRODUÇÃO_JS

---
+++ introducao-JS/script.js
@@ -0,0 +1,16 @@
+const idade =17
+//crie uma variavel para cada um dos 
+//dados listados (nome,sobrenome,telefone,casado,true ou false)
+//imprima cada um dos valores com console.log<variavel>)
+
+console.log(idade)
+
+const nome = "jonathan"
+const sobrenome="cordeiro"
+const telefone= 115
+const casado=true
+
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(true)
