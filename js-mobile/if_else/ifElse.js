211f53d36e03bf90f70a977377f7912a78985c64
l3tycya <leticordeiro08@gmail.com>
2024-10-17 12:19:07 -0300

    JS_MOBILE

---
+++ js-mobile/if_else/ifElse.js
@@ -0,0 +1,35 @@
+alert("oi")
+
+//if/ELSE -> ETRUTURA CONDICIONAL PARA TOMADA DE DECISÃO
+
+const idade = 18;
+
+if (idade > 18) {
+  console.log("maior de idade");
+}
+
+else if (idade < 18) {
+  console.log("menor de idade");
+}
+else {
+  console.log("idade inválida");
+}
+//refatoração reaer com melhorias
+//if,if para if/else if
+
+/**
+ *prompt("dakdjfakd")!!!
+ * escreva um código que pergunte qual a cor preerida, se for vermelho ,imprima"vermelho", se for azul,imprima"azul",
+ * senão não conheço essa cor
+ */
+//const cor ='verde'
+const cor = prompt('qual sua cor preferida? ')
+if (cor == 'vermelho') {
+  console.log('vermelho')
+} else if (cor == 'azul') {
+  console.log('azul')
+} else {
+  console.log('não conheço esa cor')
+}
+conole.log(cor length)//comprimento da string
+
